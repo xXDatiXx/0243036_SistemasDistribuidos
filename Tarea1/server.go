8b4fc6439af8234637262177158927e5163de4ee
Dati <102495187+xXDatiXx@users.noreply.github.com>
2024-08-15 19:00:05 -0600

    server1

---
+++ Tarea1/server.go
@@ -0,0 +1,20 @@
+/*
+1. Crear un modulo de go, será nuestro principal repositorio de trabajo
+2. Crear un archivo llamado server.go
+3. En el server.go
+    1. Agregar lógica para recibir peticiones http
+    2. Tener un endpoint para agregar un usuario
+    3. Otro endpoint para obtener la información
+    4. La información que se mande y que se reciba tiene que empaquetarse en un JSON
+    https://www.youtube.com/watch?v=5BIylxkudaE
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
